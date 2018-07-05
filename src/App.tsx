16233edb1b96fe002b5afb15c30a9744db3fb2ae
Duc Anh Phi <ducanh.phi@hybridheroes.de>
2018-07-05 22:12:08 +0200

    feat(app): add board and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,61 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
+import ImageTile from "./assets/tile.png";
+import ImageCircle from "./assets/circle.png";
+import ImageCross from "./assets/cross.png";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
-
+const screenWidth = Dimensions.get("window").width;
+const tileSize = screenWidth / 15;
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Your turn!</Text>
+        <View style={styles.board}>
+          {[...Array(15)].map((v, i) => (
+            <View style={styles.row} key={i}>
+              {[...Array(15)].map((val, index) => (
+                <TouchableOpacity
+                  key={index}
+                  style={{
+                    position: "relative"
+                  }}
+                >
+                  <Image
+                    source={ImageTile}
+                    style={{
+                      width: tileSize,
+                      height: tileSize
+                    }}
+                  />
+                  {/* <Image
+                    source={ImageCross}
+                    style={{
+                      width: tileSize,
+                      height: tileSize,
+                      position: "absolute"
+                    }}
+                  />
+                  <Image
+                    source={ImageCircle}
+                    style={{
+                      width: tileSize,
+                      height: tileSize,
+                      position: "absolute"
+                    }}
+                  /> */}
+                </TouchableOpacity>
+              ))}
+            </View>
+          ))}
+        </View>
       </View>
     );
   }
@@ -23,7 +64,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    backgroundColor: "white",
     flex: 1,
     justifyContent: "center"
   },
@@ -36,5 +77,15 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#333333",
     marginBottom: 5
+  },
+  board: {
+    flex: 1
+  },
+  row: {
+    flexDirection: "row"
+  },
+  field: {
+    height: tileSize,
+    width: tileSize
   }
 });
