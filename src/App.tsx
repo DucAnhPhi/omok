479e1603400ab886be0963fec2dcac73e0496712
Duc Anh Phi <ducanh.phi@hybridheroes.de>
2018-07-05 23:33:56 +0200

    feat(app): add ui

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import {
 import ImageTile from "./assets/tile.png";
 import ImageCircle from "./assets/circle.png";
 import ImageCross from "./assets/cross.png";
+import ImageTime from "./assets/time.png";
 
 const screenWidth = Dimensions.get("window").width;
 const tileSize = screenWidth / 15;
@@ -17,7 +18,56 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Your turn!</Text>
+        <Text style={styles.notification}>YOUR TURN</Text>
+        <View style={styles.players}>
+          <View style={[styles.playerCard, { borderColor: "red" }]}>
+            <View style={styles.playerHeader}>
+              <Text style={styles.playerName}>duc</Text>
+            </View>
+            <View style={styles.playerStats}>
+              <View style={styles.playerType}>
+                <Image
+                  source={ImageCross}
+                  style={{ width: tileSize, height: tileSize }}
+                />
+              </View>
+              <View style={styles.playerTime}>
+                <Text style={{ color: "red", fontSize: 20 }}> 5:00</Text>
+                <Image source={ImageTime} style={{ width: 20, height: 20 }} />
+              </View>
+            </View>
+          </View>
+          <View style={styles.playerCard}>
+            <View style={styles.playerHeader}>
+              <Text style={styles.playerName}>david</Text>
+            </View>
+            <View style={styles.playerStats}>
+              <View style={styles.playerType}>
+                <Image
+                  source={ImageCircle}
+                  style={{ width: tileSize, height: tileSize }}
+                />
+              </View>
+              <View style={styles.playerTime}>
+                <Text style={{ color: "black", fontSize: 20 }}> 5:00</Text>
+                <Image source={ImageTime} style={{ width: 20, height: 20 }} />
+              </View>
+            </View>
+          </View>
+        </View>
+        <View style={styles.actionButtons}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonLabel}>REDO</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonLabel}>DRAW</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: "#c0392b" }]}
+          >
+            <Text style={styles.buttonLabel}>GIVE UP</Text>
+          </TouchableOpacity>
+        </View>
         <View style={styles.board}>
           {[...Array(15)].map((v, i) => (
             <View style={styles.row} key={i}>
@@ -63,23 +113,58 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
     backgroundColor: "white",
-    flex: 1,
-    justifyContent: "center"
+    alignItems: "center",
+    flex: 1
   },
-  welcome: {
+  notification: {
+    marginTop: 15,
     fontSize: 20,
     textAlign: "center",
-    margin: 10
+    color: "black"
   },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+  players: {
+    marginTop: 20,
+    flexDirection: "row",
+    width: 270,
+    justifyContent: "space-between"
+  },
+  playerCard: {
+    borderWidth: 1,
+    width: 120,
+    borderRadius: 4,
+    borderColor: "black"
+  },
+  playerHeader: {
+    height: 25,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderColor: "black"
+  },
+  playerName: {
+    fontSize: 20,
+    color: "black"
+  },
+  playerStats: {
+    flexDirection: "row"
+  },
+  playerType: {
+    flex: 0.3,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRightWidth: 1
+  },
+  playerTime: {
+    flex: 0.7,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row"
   },
   board: {
-    flex: 1
+    flex: 1,
+    marginTop: 20
   },
   row: {
     flexDirection: "row"
@@ -87,5 +172,22 @@ const styles = StyleSheet.create({
   field: {
     height: tileSize,
     width: tileSize
+  },
+  actionButtons: {
+    marginTop: 20,
+    flexDirection: "row",
+    width: 270,
+    justifyContent: "space-between"
+  },
+  button: {
+    borderRadius: 4,
+    width: 80,
+    height: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black"
+  },
+  buttonLabel: {
+    color: "white"
   }
 });
