8e77a2f4b98f5a9f4e9418fdcf2ea2524e2c7a90
DucAnhPhi <ducanhphi1@gmail.com>
2018-08-05 01:11:10 +0200

    feat(onlinegame): simplify state and add loading indicator

--- src/views/OnlineGameView/index.tsx
+++ src/views/OnlineGameView/index.tsx
@@ -21,10 +21,15 @@ interface Props {
   profile: IProfile;
 }
 interface State {
-  loading: boolean;
-  game: any;
   hasTurn: boolean;
   isPlayer1: boolean;
+  time: number;
+  opponent: {
+    name: string;
+    points: number;
+    time: number;
+  };
+  gameId: string;
 }
 
 const convertToPositions = (moves: { [key: string]: boolean }) => {
@@ -46,13 +51,14 @@ class OnlineGameView extends React.Component<Props, State> {
   gameSocket: SocketIOClient.Socket;
   timerRef: number;
 
-  constructor() {
+  constructor(props) {
     super(undefined);
     this.state = {
-      loading: false,
-      game: null,
+      gameId: props.gameId || null,
       hasTurn: null,
-      isPlayer1: false
+      isPlayer1: false,
+      time: null,
+      opponent: null
     };
   }
 
@@ -66,33 +72,40 @@ class OnlineGameView extends React.Component<Props, State> {
       this.gameSocket.emit("joinGame", { gameId, user: profile });
     }
     this.gameSocket.on("gameCreated", initialGame => {
-      this.setState({ isPlayer1: true, game: initialGame });
+      this.setState({
+        isPlayer1: true,
+        gameId: initialGame.gameId,
+        time: initialGame.time
+      });
     });
     this.gameSocket.on("gameJoined", initialGame => {
       console.log(initialGame);
-      this.setState({ game: initialGame });
+      this.setState({
+        opponent: {
+          name: initialGame.player1Name,
+          points: initialGame.player1Points,
+          time: initialGame.time
+        }
+      });
     });
     this.gameSocket.on("move", () => {
       this.setState({ hasTurn: true });
       this.timerRef = setInterval(() => {
-        this.gameSocket.emit("tick", { gameId: this.state.game.gameId });
+        this.gameSocket.emit("tick", { gameId: this.state.gameId });
       }, 1000);
     });
     this.gameSocket.on(
       "timeUpdated",
       (params: { time: number; isPlayer1: boolean }) => {
-        if (params.isPlayer1) {
+        if (params.isPlayer1 === this.state.isPlayer1) {
           this.setState({
-            game: {
-              ...this.state.game,
-              player1Time: params.time
-            }
+            time: params.time
           });
         } else {
           this.setState({
-            game: {
-              ...this.state.game,
-              player2Time: params.time
+            opponent: {
+              ...this.state.opponent,
+              time: params.time
             }
           });
         }
@@ -108,60 +121,44 @@ class OnlineGameView extends React.Component<Props, State> {
 
   makeMove = () => {
     this.setState({ hasTurn: false });
-    this.gameSocket.emit("move", { gameId: this.state.game.gameId });
+    this.gameSocket.emit("move", { gameId: this.state.gameId });
     clearInterval(this.timerRef);
   };
 
   playerReady() {
-    const { game } = this.state;
-    if (game) {
-      this.gameSocket.emit("playerReady", { gameId: game.gameId });
+    const { gameId } = this.state;
+    if (gameId) {
+      this.gameSocket.emit("playerReady", { gameId });
     }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        {this.state.game && (
-          <View style={styles.players}>
+        <View style={styles.players}>
+          <PlayerStats
+            name={this.props.profile.username}
+            points={this.props.profile.points}
+            time={this.state.time}
+            hasTurn={this.state.hasTurn}
+            isPlayer1={this.state.isPlayer1}
+          />
+          {this.state.opponent && (
             <PlayerStats
-              name={this.props.profile.username}
-              points={this.props.profile.points}
-              time={
-                this.state.isPlayer1
-                  ? this.state.game.player1Time
-                  : this.state.game.player2Time
-              }
-              hasTurn={this.state.hasTurn}
-              isPlayer1={this.state.isPlayer1}
-            />
-            <PlayerStats
-              name={
-                this.state.isPlayer1
-                  ? this.state.game.player2Name
-                  : this.state.game.player1Name
-              }
-              points={
-                this.state.isPlayer1
-                  ? this.state.game.player2Points
-                  : this.state.game.player1Points
-              }
-              time={
-                this.state.isPlayer1
-                  ? this.state.game.player2Time
-                  : this.state.game.player1Time
-              }
+              name={this.state.opponent.name}
+              points={this.state.opponent.points}
+              time={this.state.opponent.time}
               hasTurn={this.state.hasTurn === false}
               isPlayer1={!this.state.isPlayer1}
             />
-          </View>
-        )}
-        {this.state.loading && (
-          <View style={styles.loadingIndicator}>
-            <ActivityIndicator size={"large"} color={"#8FB9A8"} />
-            {/* <Text>{this.state.loadingMessage}</Text> */}
-          </View>
-        )}
+          )}
+          {!this.state.opponent && (
+            <View style={styles.loadingIndicator}>
+              <Text style={styles.loadingText}>Waiting for opponent...</Text>
+              <ActivityIndicator size={"small"} color={"#8FB9A8"} />
+            </View>
+          )}
+        </View>
         <TouchableOpacity onPress={() => this.playerReady()}>
           <Text>READY</Text>
         </TouchableOpacity>
@@ -200,9 +197,20 @@ const styles = StyleSheet.create({
     justifyContent: "space-between"
   },
   loadingIndicator: {
-    height: 100,
+    borderWidth: 5,
+    borderRadius: 5,
+    borderColor: "rgba(0,0,0,0.2)",
+    width: 145,
+    marginHorizontal: 10,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  loadingText: {
+    fontSize: 13,
+    fontWeight: "600",
+    width: 70,
+    marginRight: 10
   }
 });
 
